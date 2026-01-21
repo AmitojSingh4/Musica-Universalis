084917f3c1575bb622895d0b6950b2d18288e3e8
Amitoj <amitojsingh4153@gmail.com>
2026-01-21 14:21:53 +0000

    Added driving to shm
    Added support for a driving force and driving angular frequency

--- shm/shm.cpp
+++ shm/shm.cpp
@@ -24,17 +24,19 @@ int main() {
 void eulerIntegration( const double initialVelocity, const double initialPosition, ofstream &file ) {
     // initial velocity (meters/seccond)
     // initial position (meters)
-    double deltaTime = 0.001; // delta time between steps (secconds)
-    double time      = 0.0;     // time (secconds)
-    double velocity  = initialVelocity;
-    double position  = initialPosition;
-    double mass      = 1.0; // mass (kilograms)
-    double k         = 1.0; // spring constant (newton/meters)
-    double dampingConstant = 1.0; // damping constant
+    double deltaTime               = 0.001; // delta time between steps (secconds)
+    double time                    = 0.0;   // time (secconds)
+    double velocity                = initialVelocity;
+    double position                = initialPosition;
+    double mass                    = 1.0; // mass (kilograms)
+    double k                       = 1.0; // spring constant (newton/meters)
+    double dampingConstant         = 1.0; // damping constant
+    double drivingForce            = 1.0; // driving force
+    double drivingAngularFrequency = 1.0; // driving angular frequency
 
     while( time < 20 ) {
         file << time << "\t" << position << "\n";
-        velocity += (-k * position - dampingConstant * velocity)* deltaTime / mass;
+        velocity += ( -k * position - dampingConstant * velocity + drivingForce * cos( drivingAngularFrequency * time ) ) * deltaTime / mass;
         position += velocity * deltaTime;
         time += deltaTime;
     }
