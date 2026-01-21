bf2a2b3737d25882cdc54cd701e3da6348bab61b
Amitoj <amitojsingh4153@gmail.com>
2026-01-21 15:20:08 +0000

    Cleaned up simple harmonic motion
    Cleaned up the simple harmonic motion file to make it easier to use for others.

--- SimpleHarmonicMotion/SimpleHarmonicMotion.cpp
+++ SimpleHarmonicMotion/SimpleHarmonicMotion.cpp
@@ -3,39 +3,45 @@
 #include <iostream>
 #include <cmath>
 #include <format>
-#include <vector>
-#include <random>
 #include <fstream>
 
 using namespace std;
 
 // function prototypes
-void eulerIntegration( const double initialVelocity, const double initialPosition, ofstream &file );
+void eulerIntegration( ofstream &file );
 
 // main
 int main() {
-    ofstream data( "SimpleHarmonicMotion-data.dat" );
-    eulerIntegration( 0, 1, data );
+    string   fileName = "SimpleHarmonicMotion-data.dat"; // name of file to save data to
+    ofstream data( fileName );
+    eulerIntegration( data );
     data.close();
     return EXIT_SUCCESS;
 }
 
 // functions
-void eulerIntegration( const double initialVelocity, const double initialPosition, ofstream &file ) {
-    // initial velocity (meters/seccond)
-    // initial position (meters)
-    double deltaTime               = 0.001; // delta time between steps (secconds)
-    double time                    = 0.0;   // time (secconds)
-    double velocity                = initialVelocity;
-    double position                = initialPosition;
-    double mass                    = 1.0; // mass (kilograms)
-    double k                       = 1.0; // spring constant (newton/meters)
-    double dampingConstant         = 1.0; // damping constant
-    double drivingForce            = 1.0; // driving force
-    double drivingAngularFrequency = 1.0; // driving angular frequency
+void eulerIntegration( ofstream &file ) {
+    // initial conditions
+    const double initialVelocity = 0.0; // initial velocity (meters/seccond)
+    const double initialPosition = 1.0; // initial position (meters)
+    // time
+    double deltaTime = 0.001; // delta time between steps (secconds)
+    double time      = 0.0;   // time (secconds)
+    double timeLimit = 20.0;  // maximum time value (secconds)
+    // position and velocity values
+    double velocity = initialVelocity;
+    double position = initialPosition;
+    // oscillator variables
+    double mass = 1.0; // mass (kilograms)
+    double k    = 1.0; // spring constant (newtons/meter)
+    // damping variables
+    double dampingConstant = 1.0; // damping constant (kilograms/seccond)
+    // driving variables
+    double drivingForce            = 1.0; // driving force (newtons)
+    double drivingAngularFrequency = 1.0; // driving angular frequency (radians/seccond)
 
-    while( time < 20 ) {
-        file << time << "\t" << position << "\n";
+    while( time < timeLimit ) {
+        file << format("{}\t{}\n", time, position );
         velocity += ( -k * position - dampingConstant * velocity + drivingForce * cos( drivingAngularFrequency * time ) ) * deltaTime / mass;
         position += velocity * deltaTime;
         time += deltaTime;
