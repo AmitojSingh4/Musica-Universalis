9a3d5c9d8700285f85daa6649508b4b3039128af
Amitoj <amitojsingh4153@gmail.com>
2026-01-28 14:24:32 +0000

    namespaces
    removed using namespace std and then added std:: to anything that needed it. Better coding practice apparently

--- WavesOnStrings/WavesOnStrings.cpp
+++ WavesOnStrings/WavesOnStrings.cpp
@@ -7,24 +7,22 @@
 #include <vector>
 #include <numbers>
 
-using namespace std;
-
 // function prototypes
-vector<double> createString( const int numberOfPoints, const double length, const double height ); // plucked string
+std::vector<double> createString( const int numberOfPoints, const double length, const double height ); // plucked string
 
-vector<double> createString( const int numberOfPoints, const double length, const double height, const double width, const double startingLocation, const string sign = "positive" ); // pulse string
+std::vector<double> createString( const int numberOfPoints, const double length, const double height, const double width, const double startingLocation, const std::string sign = "positive" ); // pulse string
 
-vector<double> createString( const int numberOfPoints, const int mode, const double height ); // standing wave string
+std::vector<double> createString( const int numberOfPoints, const int mode, const double height ); // standing wave string
 
-void updateString( vector<double> &stringVector, ofstream &file, const double deltaLength );
+void updateString( std::vector<double> &stringVector, std::ofstream &file, const double deltaLength );
 
 // main
 int main() {
     // file and data saving
-    string   fileName = "WavesOnStringsData.dat"; // name of file to save data to
-    ofstream data( fileName );
+    std::string   fileName = "WavesOnStringsData.dat"; // name of file to save data to
+    std::ofstream data( fileName );
     if( !data ) {
-        cerr << format( "Error: could not open file, {}\n\n", fileName );
+        std::cerr << format( "Error: could not open file, {}\n\n", fileName );
         abort();
     }
     // creating string vector
@@ -33,8 +31,8 @@ int main() {
     const double height         = 0.1;
     // testing
     // vector<double> stringVector = createString( numberOfPoints, length, height ); // 1
-    vector<double> stringVector = createString( numberOfPoints, 3, height ); // 3
-    // vector<double> stringVector = createString( numberOfPoints, length, height, 5, 50 ); // 2
+    // vector<double> stringVector = createString( numberOfPoints, 3, height ); // 3
+    std::vector<double> stringVector = createString( numberOfPoints, length, height, 5, 50 ); // 2
     updateString( stringVector, data, length / ( numberOfPoints - 1 ) );
 
     data.close();
@@ -43,8 +41,8 @@ int main() {
 }
 
 // functions
-vector<double> createString( const int numberOfPoints, const double length, const double height ) {
-    vector<double> stringVector( numberOfPoints, 0.0 );
+std::vector<double> createString( const int numberOfPoints, const double length, const double height ) {
+    std::vector<double> stringVector( numberOfPoints, 0.0 );
     const double   gradiant = height / ( length / 2.0 ); // gradient per point on the string
     for( int i = 0; i <= ( ( numberOfPoints - 1 ) / 2 ); i++ ) {
         stringVector.at( i )                      = i * gradiant; // fills left to midpoint
@@ -53,8 +51,8 @@ vector<double> createString( const int numberOfPoints, const double length, cons
     return stringVector;
 }
 
-vector<double> createString( const int numberOfPoints, const double length, const double height, const double width, const double startingLocation, const string sign ) {
-    vector<double> stringVector( numberOfPoints, 0.0 );
+std::vector<double> createString( const int numberOfPoints, const double length, const double height, const double width, const double startingLocation, const std::string sign ) {
+    std::vector<double> stringVector( numberOfPoints, 0.0 );
     double         signValue = 1.0;
     if( sign != "positive" ) {
         signValue = -1.0;
@@ -62,47 +60,47 @@ vector<double> createString( const int numberOfPoints, const double length, cons
     const int widthPoints           = int( ( width / length ) * ( numberOfPoints - 1.0 ) + 0.5 );          // the width in terms of points on the string
     const int startingLocationPoint = int( ( startingLocation / length ) * ( numberOfPoints - 1 ) + 0.5 ); // starting location in the vector
     for( int i = startingLocationPoint; i <= startingLocationPoint + widthPoints; i++ ) {
-        stringVector.at( i ) = height * signValue * sin( ( i - startingLocationPoint ) * 2.0 * numbers::pi / widthPoints );
+        stringVector.at( i ) = height * signValue * sin( ( i - startingLocationPoint ) * 2.0 * std::numbers::pi / widthPoints );
     }
     return stringVector;
 }
 
-vector<double> createString( const int numberOfPoints, const int mode, const double height ) {
-    vector<double> stringVector( numberOfPoints, 0.0 );
+std::vector<double> createString( const int numberOfPoints, const int mode, const double height ) {
+    std::vector<double> stringVector( numberOfPoints, 0.0 );
     for( int i = 0; i < stringVector.size(); i++ ) {
-        double heightValue   = height * sin( i * ( mode / 2.0 ) * 2.0 * numbers::pi / ( numberOfPoints - 1 ) );
+        double heightValue   = height * sin( i * ( mode / 2.0 ) * 2.0 * std::numbers::pi / ( numberOfPoints - 1 ) );
         stringVector.at( i ) = heightValue;
     }
     return stringVector;
 }
 
-void updateString( vector<double> &stringVector, ofstream &file, const double deltaLength ) {
+void updateString( std::vector<double> &stringVector, std::ofstream &file, const double deltaLength ) {
     // time
     double       deltaTime = 0.1;  // delta time between steps (secconds)
     double       time      = 0.0;  // time (secconds)
-    const double timeLimit = 20.0; // maximum time value (secconds)
+    const double timeLimit = 50.0; // maximum time value (secconds)
     // string variables
     const int      stringPoints = stringVector.size();
     const double   tension      = 10.0;       // tension along the string (newtons)
-    vector<double> mass( stringPoints, 1.0 ); // mass of the string (kg) - mass is uniform accross the string
+    std::vector<double> mass( stringPoints, 1.0 ); // mass of the string (kg) - mass is uniform accross the string
     // temporary vectors
-    vector<double> velocity( stringPoints, 0.0 );
-    vector<double> temporaryString( stringPoints, 0.0 );
+    std::vector<double> velocity( stringPoints, 0.0 );
+    std::vector<double> temporaryString( stringPoints, 0.0 );
     temporaryString.at( 0 )                = stringVector.at( 0 );
     temporaryString.at( stringPoints - 1 ) = stringVector.at( stringPoints - 1 );
 
     file << "t\tx\ty\n";
     while( time <= timeLimit ) {
-        file << format( "{}\t{}\t{}\n", time, 0, stringVector.at( 0 ) );
+        file << std::format( "{}\t{}\t{}\n", time, 0, stringVector.at( 0 ) );
         for( int i = 1; i < stringPoints - 1; i++ ) {
-            file << format( "{}\t{}\t{}\n", time, i * deltaLength, stringVector.at( i ) );
+            file << std::format( "{}\t{}\t{}\n", time, i * deltaLength, stringVector.at( i ) );
             velocity.at( i ) += ( ( tension / mass.at( i ) ) * ( ( stringVector.at( i - 1 ) - 2 * stringVector.at( i ) + stringVector.at( i + 1 ) ) / pow( deltaLength, 2 ) ) ) * deltaTime;
             temporaryString.at( i ) = stringVector.at( i ) + velocity.at( i ) * deltaTime;
             if( i > 2 ) {
                 stringVector.at( i - 2 ) = temporaryString.at( i - 2 );
             }
         }
-        file << format( "{}\t{}\t{}\n", time, ( stringPoints - 1 ) * deltaLength, stringVector.at( stringPoints - 1 ) );
+        file << std::format( "{}\t{}\t{}\n", time, ( stringPoints - 1 ) * deltaLength, stringVector.at( stringPoints - 1 ) );
         stringVector.at( stringPoints - 2 ) = temporaryString.at( stringPoints - 2 ); // last 2 points that dont get changed
         stringVector.at( stringPoints - 3 ) = temporaryString.at( stringPoints - 3 );
         time += deltaTime;
